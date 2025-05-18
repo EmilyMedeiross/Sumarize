b5709fbbf4bd8dd01313a77e67310eca27686ec1
EmilyMedeiross <emilysantosjprn@gmail.com>
2025-05-18 11:58:54 -0300

    main.py  & models.py

---
+++ Sumarize/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+from Sumarize.models import Resumo
+from typing import List
+
+app = FastAPI()
+
+resumos: List[Resumo] = []
+
+@app.post("/resumir/", response_model=Resumo)
+def create_resumo(resumo: Resumo):
+    if not resumo.texto:
+        raise  HTTPException(status_code=400, detail="O texto anexado não pode ser vazio!!" )
+    
+    if len(resumo.texto) > 3000: 
+        raise HTTPException(status_code= 400, detail= "Texto muito longo, reduza a quantidade de caracteres para 4000")
+    
+    resumos.append(resumo)
+    return resumo
+
+@app.get("/resumos/", response_model=List[Resumo])
+def listar_resumos():
+    return resumos
